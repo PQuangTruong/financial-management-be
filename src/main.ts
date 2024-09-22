46acf847def15d4b9e6f13c2011ab2320ebe7cd7
PQuangTruong <pquangtruong2002@gmail.com>
2024-09-22 14:07:36 +0700

    update user 2

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,14 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      //add more domains
+    ],
+    credentials: true,
+  });
+
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
 
